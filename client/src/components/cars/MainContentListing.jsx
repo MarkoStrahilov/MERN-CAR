f9f15f5f3e73ef12c14cca09070ec90645c7759e
Marko Strahilov <strahilovmarko@yahoo.com>
2022-04-19 23:35:05 +0200

    Update MainContentListing.jsx

--- client/src/components/cars/MainContentListing.jsx
+++ client/src/components/cars/MainContentListing.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {HiLocationMarker,HiOutlineLocationMarker} from 'react-icons/hi'
+import {BiCurrentLocation} from 'react-icons/bi'
 
 import '../../assets/listings.css'
 
@@ -11,6 +13,20 @@ const MainContentListing = ({data}) => {
         <div className="mt-10 mb-10">
           <label className="block text-gray-700 text-sm font-bold mb-2">DESCRIPTION</label>
             <p>{data?.description}</p>
+        </div>
+        <div className="mt-10 mb-6 flex items-center">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Location</label>
+          <BiCurrentLocation size={30} style={{marginLeft: '.5rem', marginBottom: '.5rem'}}/>
+        </div>
+        <div className="flex items-center justify-around">
+          <div className='flex items-center'>
+          <HiOutlineLocationMarker size={30}/>
+          Location Pickup - {data?.location?.pickup}
+          </div>
+          <div className='flex items-center'>
+          <HiLocationMarker size={30}/>
+          Location Return - {data?.location?.return}
+          </div>
         </div>
     </div>
   )
