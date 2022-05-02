f6ece4756782d35ddc6222ab7c65ccc19e5a8f8d
Marko Strahilov <strahilovmarko@yahoo.com>
2022-05-02 21:38:18 +0200

    update / user-details show listings

--- client/src/components/users/accountDetails/ProfileHeading.jsx
+++ client/src/components/users/accountDetails/ProfileHeading.jsx
@@ -11,7 +11,7 @@ const ProfileHeading = ({info}) => {
        <div className='user-df-info'>
        <img src={'https://www.clipartmax.com/png/middle/296-2969961_no-image-user-profile-icon.png'} className='profile-img' alt="user" />
         <div>
-        <p className='font-bold text-2xl mb-1'>{info?.username}</p>
+        <h1 className='font-bold text-2xl mb-1'>{info?.username}</h1>
           Joined {moment(info?.createdAt).format('LL')}
         </div>
        </div>
--- client/src/components/users/accountDetails/ProfileMain.jsx
+++ client/src/components/users/accountDetails/ProfileMain.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const ProfileMain = ({info}) => {
 
-  console.log(info)
-
   return (
-    <div className='container mx-auto'>
-       {info?.carListings.length !== 0 ? <p>see {info?.username}'s available car listings</p> : <p>{info?.username} doesn't have listings available</p>}
+    <div className='container mx-auto mt-20'>
+       {info?.carListings.length !== 0 ? <p className='text-5xl mb-10'>See {info?.username}'s available car listings</p> : <p className='text-5xl'>{info?.username} doesn't have listings available</p>}
+      <div className="user-details-listing-card flex flex-wrap">
       {info?.carListings.length !== 0 && info.carListings.map((listing) => (
-        <div className='card'>
-          <p className='mt-5 mb-5'>user listings</p>
-          <h1>{listing?.name}</h1>
-        </div>  
+        <div className="card w-96 bg-base-100 shadow-xl mt-10 mb-5" key={listing?._id}>
+        <figure><img src={listing?.images[0]} alt="Shoes" /></figure>
+        <div className="card-body">
+          <h2 className="card-title">{listing?.name}</h2>
+          <p>{listing?.description}</p>
+          <div className="card-actions justify-end">
+            <Link to={`/car-listing/${listing?._id}`} className="btn btn-primary">Visit Listing</Link>
+          </div>
+        </div>
+      </div>  
       ))}
+      </div>
     </div>
   )
 }
